62679b1644dff0054e1d477ed34ecef092d7772a
Guillermo Morton <guillermort2000@gmail.com>
2021-06-14 19:55:17 -0300

    Se actualizo el sistema, reemplazamos la forma convencional de validar el formulario para usar una libreria llamada Formik

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,81 +1,89 @@
-import React, { useState, useEffect } from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState, useEffect, useRef } from "react";
+import { Button } from "react-bootstrap";
+import { useFormik, Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from 'yup';
 
 const Login = () => {
-  // const token= '4060184407368673'
-  // const URL=`https://superheroapi.com/api/${token}/`
+    // const token= '4060184407368673'
+    // const URL=`https://superheroapi.com/api/${token}/`
 
-  // Definimos la URL
-  const URL = "http://challenge-react.alkemy.org/";
-  // Definimos Axios
-  const axios = require("axios").default;
-  // Definimos el token
-  const [token, setToken] = useState("");
-  const [logged, setlogged] = useState(false);
-  // Definimos los state de los input
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+    // Definimos la URL
+    const URL = "http://challenge-react.alkemy.org/";
+    // Definimos Axios
+    const axios = require("axios").default;
+    // Definimos el token
+    const [token, setToken] = useState("");
+    const [passed, setPassed] = useState(false);
+    // Definimos los state de los input
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (
-      (email != "" || pass != "") &&
-      email === "challenge@alkemy.org" &&
-      pass === "react"
-    ) {
-      // Pasa la validacion, hacer un post a la api
-      axios
-        .post(URL, {
-          email: email,
-          password: pass,
-        })
-        .then(function (response) {
-          setToken(response.data.token);
-          setlogged(true);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    } else {
-      alert("datos invalidos");
-      setToken('');
-      setlogged(false);
-    }
-  };
+    //   use Effect que solo actua en la actualizacion
+  const isInitialMount = useRef(true);
   useEffect(() => {
-    localStorage.setItem("tokenKey", JSON.stringify(token));
-  }, [token]);
-  return (
-    <div className="container">
-      <div class="w-50 mx-auto my-5">
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              placeholder="Enter email"
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+        if (email === "challenge@alkemy.org" && pass === "react") {
+            // Pasa la validacion, hacer un post a la api
+            axios.post(URL, {
+                    email: email,
+                    password: pass,
+                })
+                .then(function (response) {
+                    setToken(response.data.token);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }else{
+            console.log('Datos incorrectos')
+        }
+        localStorage.setItem("tokenKey", JSON.stringify(token));
+    }
+  },[token, passed]);
+    return (
+        <div className="container">
+            <div class="w-50 mx-auto my-5">
+                <Formik
+                    initialValues={{ pass: '', email: '' }}
+                    validationSchema={Yup.object({
+                        email: Yup.string().email('Invalid email address').required('Required'),
+                        pass: Yup.string()
+                            .min(5, 'Must be 5 characters or more')
+                            .required('Required'),
+                    })}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            setEmail(values.email);
+                            setPass(values.pass);
+                            setSubmitting(false);
+                        }, 390);
+                        setTimeout(()=>{
+                            setPassed(!passed)
+                        },400)
+                    }}
+                >
+                    <Form>
+                        <div className='form-group'>
+                            <label htmlFor="email">Email Address</label>
+                            <Field className='form-control' name="email" type="email" />
+                            <ErrorMessage name="email" />
+                        </div>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              onChange={(e) => setPass(e.target.value)}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
-    </div>
-  );
+                        <div className='form-group'>
+                            <label htmlFor="firstName">Password</label>
+                            <Field className='form-control' name="pass" type="password" />
+                            <ErrorMessage name="pass" />
+                        </div>
+
+                        <Button type="submit">Submit</Button>
+                    </Form>
+                </Formik>
+
+            </div>
+        </div>
+    );
 };
 
 export default Login;
