392446cc43ce66dfc1248468a486c5dc57e6f7e4
Guillermo Morton <guillermort2000@gmail.com>
2021-06-14 23:59:10 -0300

    Se avanzo con la logica del buscador, el usuario ya puede buscar un heroe por su nombre, y recibir una card con su foto, nombre, si es heroe o villano y su cualidad mas destacable

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,89 +1,90 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Button } from "react-bootstrap";
-import { useFormik, Formik, Field, Form, ErrorMessage } from "formik";
-import * as Yup from 'yup';
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
-    // const token= '4060184407368673'
-    // const URL=`https://superheroapi.com/api/${token}/`
+  // const token= '4060184407368673'
+  // const URL=`https://superheroapi.com/api/${token}/`
 
-    // Definimos la URL
-    const URL = "http://challenge-react.alkemy.org/";
-    // Definimos Axios
-    const axios = require("axios").default;
-    // Definimos el token
-    const [token, setToken] = useState("");
-    const [passed, setPassed] = useState(false);
-    // Definimos los state de los input
-    const [email, setEmail] = useState("");
-    const [pass, setPass] = useState("");
+  // Definimos la URL
+  const URL = "http://challenge-react.alkemy.org/";
+  // Definimos Axios
+  const axios = require("axios").default;
+  // Definimos el token
+  const [token, setToken] = useState("");
+  const [passed, setPassed] = useState(false);
+  // Definimos los state de los input
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
-    //   use Effect que solo actua en la actualizacion
+  //   use Effect que solo actua en la actualizacion
   const isInitialMount = useRef(true);
   useEffect(() => {
     if (isInitialMount.current) {
       isInitialMount.current = false;
     } else {
-        if (email === "challenge@alkemy.org" && pass === "react") {
-            // Pasa la validacion, hacer un post a la api
-            axios.post(URL, {
-                    email: email,
-                    password: pass,
-                })
-                .then(function (response) {
-                    setToken(response.data.token);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        }else{
-            console.log('Datos incorrectos')
-        }
-        localStorage.setItem("tokenKey", JSON.stringify(token));
+      if (email === "challenge@alkemy.org" && pass === "react") {
+        // Pasa la validacion, hacer un post a la api
+        axios.post(URL, {
+            email: email,
+            password: pass,
+          })
+          .then(function (response) {
+            setToken(response.data.token);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      } else {
+        console.log("Datos incorrectos");
+      }
+      localStorage.setItem("tokenKey", JSON.stringify(token));
     }
-  },[token, passed]);
-    return (
-        <div className="container">
-            <div class="w-50 mx-auto my-5">
-                <Formik
-                    initialValues={{ pass: '', email: '' }}
-                    validationSchema={Yup.object({
-                        email: Yup.string().email('Invalid email address').required('Required'),
-                        pass: Yup.string()
-                            .min(5, 'Must be 5 characters or more')
-                            .required('Required'),
-                    })}
-                    onSubmit={(values, { setSubmitting }) => {
-                        setTimeout(() => {
-                            setEmail(values.email);
-                            setPass(values.pass);
-                            setSubmitting(false);
-                        }, 390);
-                        setTimeout(()=>{
-                            setPassed(!passed)
-                        },400)
-                    }}
-                >
-                    <Form>
-                        <div className='form-group'>
-                            <label htmlFor="email">Email Address</label>
-                            <Field className='form-control' name="email" type="email" />
-                            <ErrorMessage name="email" />
-                        </div>
-
-                        <div className='form-group'>
-                            <label htmlFor="firstName">Password</label>
-                            <Field className='form-control' name="pass" type="password" />
-                            <ErrorMessage name="pass" />
-                        </div>
-
-                        <Button type="submit">Submit</Button>
-                    </Form>
-                </Formik>
-
+  }, [token, passed]);
+  return (
+    <div className="container">
+      <div class="w-50 mx-auto my-5">
+        <Formik
+          initialValues={{ pass: "", email: "" }}
+          validationSchema={Yup.object({
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Required"),
+            pass: Yup.string()
+              .min(5, "Must be 5 characters or more")
+              .required("Required"),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              setEmail(values.email);
+              setPass(values.pass);
+              setSubmitting(false);
+            }, 390);
+            setTimeout(() => {
+              setPassed(!passed);
+            }, 400);
+          }}
+        >
+          <Form>
+            <div className="form-group">
+              <label htmlFor="email">Email Address</label>
+              <Field className="form-control" placeholder='example@example.com' name="email" type="email" />
+              <ErrorMessage name="email" />
             </div>
-        </div>
-    );
+
+            <div className="form-group">
+              <label htmlFor="firstName">Password</label>
+              <Field className="form-control" placeholder='Write your password' name="pass" type="password" />
+              <ErrorMessage name="pass" />
+            </div>
+
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,34 +1,121 @@
-import React from "react";
-import { Form, Button, Card } from "react-bootstrap";
+import React, { useState, useEffect, useRef } from "react";
+import { Button, Card } from "react-bootstrap";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Search = () => {
+  // Base URL
+  const URL = `https://www.superheroapi.com/api.php/4060184407368673/`;
+
+  const [hero, setHeroSearched] = useState(undefined);
+  const [mainPs, setMainPs] = useState("");
+  const [maxPsNum, setMaxPsNum] = useState("");
+  const [psValues, setPsValues] = useState([]);
+  const [psNames, setPsNames] = useState([]);
+  // Definimos Axios
+  const axios = require("axios").default;
+
+  const findMainPS = () => {
+    // Obtenemos la estadistica mas alta
+    setMaxPsNum(
+      Math.max(
+        hero.powerstats.intelligence,
+        hero.powerstats.strength,
+        hero.powerstats.speed,
+        hero.powerstats.durability,
+        hero.powerstats.power,
+        hero.powerstats.combat
+      )
+    );
+    // obtenemos los valores de todas las estadisticas
+    setPsValues(Object.values(hero.powerstats));
+    // obtenemos los nombres de las propiedades como strings
+    setPsNames(Object.getOwnPropertyNames(hero.powerstats));
+
+    /*
+        Como psValues y psNames, comparten el mismo orden, mediante array.indexOf(value),
+        podemos encontrar la posicion en el arreglo del valor maximo psValues.indexOf(maxPsNum.toString()).
+        Convertimos este valor a string debido a que los datos de psValues, son strings.
+
+        De esta forma podemos extraer el nombre de la propiedad del objeto con el numero mas alto:
+          setState(ArrayOfNames[ArrayofValues.indexOf(maxNumber.toString())])
+          setMainPs(psNames[psValues.indexOf(maxPsNum.toString())]);
+    */
+  };
+
+  //   use Effect que solo actua en la actualizacion
+  const isInitialMount = useRef(true);
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      findMainPS();
+    }
+  }, [hero]);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      setMainPs(psNames[psValues.indexOf(maxPsNum.toString())]);
+    }
+  }, [psNames]);
   return (
     <div>
       <div className="container">
-        <div class="w-50 mx-auto my-5">
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control type="text" placeholder="Search hero" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+        <div className="w-50 mx-auto my-5">
+          <Formik
+            initialValues={{ search: "" }}
+            validationSchema={Yup.object({
+              search: Yup.string().required("Required"),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                axios
+                  .get(`${URL}search/${values.search}`)
+                  .then(function (response) {
+                    // handle success
+                    console.log(response);
+                    setHeroSearched(response.data.results[0]);
+                  })
+                  .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                  })
+                  .then(function () {
+                    // always executed
+                  });
+                setSubmitting(false);
+              }, 390);
+            }}
+          >
+            <Form>
+              <div className="form-group">
+                <label htmlFor="search">Search Hero</label>
+                <Field
+                  className="form-control"
+                  placeholder="Batman"
+                  name="search"
+                  type="text"
+                />
+                <ErrorMessage name="search" />
+              </div>
+              <Button type="submit">Submit</Button>
+            </Form>
+          </Formik>
         </div>
         <section>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>HERO NAME</Card.Title>
-              <Card.Text>
-                GOOD
-              </Card.Text>
-              <Card.Text>
-                INTELIGENCE
-              </Card.Text>
-              <Button variant="success">ADD TO TEAM</Button>
-            </Card.Body>
-          </Card>
+          {hero != undefined ? (
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={hero.image.url} />
+              <Card.Body>
+                <Card.Title>{hero.name}</Card.Title>
+                <Card.Text>{hero.biography.alignment}</Card.Text>
+                <Card.Text>{mainPs}</Card.Text>
+                <Button variant="success">ADD TO TEAM</Button>
+              </Card.Body>
+            </Card>
+          ) : null}
         </section>
       </div>
     </div>
